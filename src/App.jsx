a83ff6dd293f0855965443c3546c1d0cb03728f7
alex parfenov <alexp-2004@mail.ru>
2022-04-07 00:09:36 +0700

    change some functional

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,31 @@
 import './App.css';
 import {useDispatch, useSelector} from "react-redux";
+import {addCustomerAction, removeCustomerAction} from "./store/customerReducer";
+import {addCashAction, getCashAction} from "./store/cashReducer";
 
 function App() {
   const dispatch = useDispatch()
   const cash  = useSelector(state => state.cash.cash)
+  const customers  = useSelector(state => state.customers.customers)
 
   const getCash = (money) => {
-      dispatch({type: 'GET_CASH', payload: money})
+      dispatch(getCashAction(money))
   }
 
   const addCash = (money) => {
-      dispatch({type: 'ADD_CASH', payload: money})
+      dispatch(addCashAction(money))
+  }
+
+  const addCustomer = (name) => {
+        const customer = {
+            name,
+            id: Date.now(),
+        }
+        dispatch(addCustomerAction(customer))
+  }
+
+  const removeCustomer = (customer) => {
+      dispatch(removeCustomerAction(customer.id))
   }
 
   return (
@@ -21,6 +36,11 @@ function App() {
         <div style={{display: 'flex'}}>
             <button onClick={() => addCash(Number(prompt()))}>Add cash</button>
             <button onClick={() => getCash(Number(prompt()))}>Get cash</button>
+            <button onClick={() => addCustomer(prompt())}>Add client</button>
+            <button onClick={() => getCash(Number(prompt()))}>Get client</button>
+        </div>
+        <div>
+            {customers.length > 0 ? <div>{customers.map(customer => <div onClick={() => removeCustomer(customer)}>{customer.name}</div>)}</div> : <div>Клиентов нет</div>}
         </div>
     </div>
   );
